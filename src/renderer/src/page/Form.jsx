25b74fd2173399ccd6cf7120337d8a9120971e3f
shakuneko <shakuneko47@gmail.com>
2023-09-08 13:40:56 +0800

    Add_value_test

--- src/renderer/src/page/Form.jsx
+++ src/renderer/src/page/Form.jsx
@@ -14,30 +14,33 @@ class From extends Component {
     e.preventDefault();
     console.log(this.state.value)
   }
-  onChangehandle=(e)=>{
-    this.setState({
-      value:e.target.value
-    })
-  }
+  // onChangehandle=(e)=>{
+  //   this.setState({
+  //     value:e.target.value
+  //   })
+  // }
 start = {
   user:{
-    salary: '',
-    remark: '',
+    coach:'',     //教練
+    stu1:'',      //學生1
+    stu2:'',      //學生2
+    number:'',    //堂數
+    salary: '',   //堂薪
+    lesson:'',    //體驗課
+    remark: '',   //備註
 
 
   }
 }
-classuser(e,props,index){
+classuser(e,props){
   var user = this.start.user
-  user[props]
+  user[props] = e.target.value
 }
 send(){
   console.log(this.start.user);
 }
 render(){
 
-
-  
   return (
     <div className="container-fluid">
       <div className="row form_class row-no-gutters">
@@ -62,7 +65,7 @@ render(){
               <div className="form-group">
                   <label  for="exampleInputEmail1">教練:</label>
                   <div className="select">
-                    <select className="form-select " aria-label="Default select example">
+                    <select className="form-select " onChange={(e)=>this.classuser(e,'coach')}>
                         <option selected>-</option>
                         <option value="1">A</option>
                         <option value="2">B</option>
@@ -74,7 +77,7 @@ render(){
                 <div className="form-group4-1"> 
                   <label for="exampleInputEmail1">學員1:</label>
                   <div className="select">
-                  <select class="form-select" aria-label="Default select example">
+                  <select class="form-select" onChange={(e)=>this.classuser(e,'stu1')}>
                       <option selected>-</option>
                       <option value="1">Lulu</option>
                       <option value="2">田晴瑄</option>
@@ -87,7 +90,7 @@ render(){
                 <div className="form-group4-1">
                     <label for="exampleInputEmail1">學員2:</label>
                     <div className="select">
-                      <select class="form-select " aria-label="Default select example">
+                      <select class="form-select " onChange={(e)=>this.classuser(e,'stu2')}>
                           <option selected>-</option>
                           <option value="1">Lulu</option>
                           <option value="2">田晴瑄</option>
@@ -99,11 +102,11 @@ render(){
               <div className="form-group">
                   <label for="exampleInputEmail1">堂數:</label>
                   <div className="select">
-                    <select class="form-select" aria-label="Default select example">
+                    <select class="form-select" onChange={(e)=>this.classuser(e,'number')}>
                         <option selected>-</option>
                         <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
+                        <option value="10">10</option>
+                        <option value="20">20</option>
                     </select>
                   </div>
               </div>
@@ -117,21 +120,24 @@ render(){
               <div class="form-group">
                   <label  className="" for="exampleInputEmail1">體驗課:</label>
                   <div className=" check">
-                    <div className="checkbox">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1" ></input>
-                        <label class="form-check-label" for="exampleCheck1">是</label>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" value='true' onChange={(e)=>this.classuser(e,'lesson')}></input>
+                      <label class="form-check-label" for="flexRadioDefault1">
+                        是
+                      </label>
                     </div>
-                    <div className="checkbox">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
-                        <label class="form-check-label" for="exampleCheck1">否</label>
-                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" value='false' onChange={(e)=>this.classuser(e,'lesson')}></input>
+                      <label class="form-check-label" for="flexRadioDefault2">
+                        否
+                      </label>
                   </div>
               </div>
-              
+              </div>
               <div class="form-group2">
                   <label for="exampleInputPassword1">備註:</label>
                   <div className="select">
-                    <textarea class="form-select" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    <textarea class="form-select" id="exampleFormControlTextarea1" rows="3" onChange={(e)=>this.classuser(e,'remark')}></textarea>
                   </div>  
               </div>
               <div class="form-group3">
