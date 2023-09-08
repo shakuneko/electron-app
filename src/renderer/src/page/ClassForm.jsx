387662d02a8307cf001582a63e7a2483557a0e7e
shakuneko <shakuneko47@gmail.com>
2023-09-08 21:47:27 +0800

    fix_segmented-control

--- src/renderer/src/page/ClassForm.jsx
+++ src/renderer/src/page/ClassForm.jsx
@@ -31,103 +31,237 @@ function ClassForm() {
                     <button className="btn btn-outline-golden" type="button" onClick={()=>updateToggle(4)}>場地租借</button>
                   </div>
               </div>
+              {/* PT課 */}
               <div className={toggle === 1 ? "show-content":"content"}>
-                <p>hihi</p>
+                <div className="class_category">
+                    <div className="form-group">
+                        <label  for="exampleInputEmail1">教練:</label>
+                        <div className="select">
+                          <select className="form-select " >
+                              <option selected>-</option>
+                              <option value="A">A</option>
+                              <option value="B">B</option>
+                              <option value="C">C</option>
+                          </select>
+                        </div>
+                    </div>
+                    <div className="form-group4">
+                      <div className="form-group4-1"> 
+                        <label for="exampleInputEmail1">學員1:</label>
+                        <div className="select">
+                        <select class="form-select" >
+                            <option selected>-</option>
+                            <option value="1">Lulu</option>
+                            <option value="2">田晴瑄</option>
+                        </select>
+                        </div>
+                        </div>
+                        <button className="btn btn-originalgray" type="button">已付費</button>
+                    </div>
+                    <div className="form-group4"> 
+                      <div className="form-group4-1">
+                          <label for="exampleInputEmail1">學員2:</label>
+                          <div className="select">
+                            <select class="form-select " >
+                                <option selected>-</option>
+                                <option value="1">Lulu</option>
+                                <option value="2">田晴瑄</option>
+                            </select>
+                          </div>
+                      </div>
+                      <button className="btn btn-originalgray" type="button">未付費</button>
+                    </div>
+                    <div className="form-group">
+                        <label for="exampleInputEmail1">堂數:</label>
+                        <div className="select">
+                          <select class="form-select" >
+                              <option selected>-</option>
+                              <option value="1">1</option>
+                              <option value="10">10</option>
+                              <option value="20">20</option>
+                          </select>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">堂薪:</label>
+                        <div className="select">
+                          <input type="text" class="form-select"></input>
+                        </div>
+                    </div>
+                    
+                    <div class="form-group">
+                        <label  className="" for="exampleInputEmail1">體驗課:</label>
+                        <div className=" check">
+                          <div class="form-check">
+                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" value='true' ></input>
+                            <label class="form-check-label" for="flexRadioDefault1">
+                              是
+                            </label>
+                          </div>
+                          <div class="form-check">
+                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" value='false' ></input>
+                            <label class="form-check-label" for="flexRadioDefault2">
+                              否
+                            </label>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="form-group2">
+                        <label for="exampleInputPassword1">備註:</label>
+                        <div className="select">
+                          <textarea class="form-select" id="exampleFormControlTextarea1" rows="3" ></textarea>
+                        </div>  
+                    </div>
+                    <div class="form-group3">
+                      <button type="submit" class="btn btn-golden" >新增</button>
+                    </div>
+                </div>
 
               </div>
+               {/* 皮拉提斯課 */}
               <div className={toggle === 2 ? "show-content":"content"}>
-                <p>hi</p>
+                <div className="class_category">
+                      <div className="form-group">
+                          <label  for="exampleInputEmail1">教練:</label>
+                          <div className="select">
+                            <select className="form-select " >
+                                <option selected>-</option>
+                                <option value="A">A</option>
+                                <option value="B">B</option>
+                                <option value="C">C</option>
+                            </select>
+                          </div>
+                      </div>
+                      <div className="form-group4">
+                        <div className="form-group4-1"> 
+                          <label for="exampleInputEmail1">學員1:</label>
+                          <div className="select">
+                          <select class="form-select" >
+                              <option selected>-</option>
+                              <option value="1">Lulu</option>
+                              <option value="2">田晴瑄</option>
+                          </select>
+                          </div>
+                          </div>
+                          <button className="btn btn-originalgray" type="button">已付費</button>
+                      </div>
+                      <div className="form-group4"> 
+                        <div className="form-group4-1">
+                            <label for="exampleInputEmail1">學員2:</label>
+                            <div className="select">
+                              <select class="form-select " >
+                                  <option selected>-</option>
+                                  <option value="1">Lulu</option>
+                                  <option value="2">田晴瑄</option>
+                              </select>
+                            </div>
+                        </div>
+                        <button className="btn btn-originalgray" type="button">未付費</button>
+                      </div>
+                      <div className="form-group">
+                          <label for="exampleInputEmail1">堂數:</label>
+                          <div className="select">
+                            <select class="form-select" >
+                                <option selected>-</option>
+                                <option value="1">1</option>
+                                <option value="10">10</option>
+                                <option value="20">20</option>
+                            </select>
+                          </div>
+                      </div>
+                      <div class="form-group">
+                          <label for="exampleInputEmail1">堂薪:</label>
+                          <div className="select">
+                            <input type="text" class="form-select"></input>
+                          </div>
+                      </div>
+                      
+                      <div class="form-group">
+                          <label  className="" for="exampleInputEmail1">體驗課:</label>
+                          <div className=" check">
+                            <div class="form-check">
+                              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" value='true' ></input>
+                              <label class="form-check-label" for="flexRadioDefault1">
+                                是
+                              </label>
+                            </div>
+                            <div class="form-check">
+                              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" value='false' ></input>
+                              <label class="form-check-label" for="flexRadioDefault2">
+                                否
+                              </label>
+                          </div>
+                      </div>
+                      </div>
+                      <div class="form-group2">
+                          <label for="exampleInputPassword1">備註:</label>
+                          <div className="select">
+                            <textarea class="form-select" id="exampleFormControlTextarea1" rows="3" ></textarea>
+                          </div>  
+                      </div>
+                      <div class="form-group3">
+                        <button type="submit" class="btn btn-golden" >新增</button>
+                      </div>
+                  </div>
 
               </div>
+              {/* 團課 */}
               <div className={toggle === 3 ? "show-content":"content"}>
                 <p>bye</p>
 
               </div>
-              <div className={toggle === 4 ? "show-content":"content"}>
-                <p>byebye</p>
-
-              </div>
-              <div className="form-group">
-                  <label  for="exampleInputEmail1">教練:</label>
-                  <div className="select">
-                    <select className="form-select " >
-                        <option selected>-</option>
-                        <option value="A">A</option>
-                        <option value="B">B</option>
-                        <option value="C">C</option>
-                    </select>
-                  </div>
-              </div>
-              <div className="form-group4">
-                <div className="form-group4-1"> 
-                  <label for="exampleInputEmail1">學員1:</label>
-                  <div className="select">
-                  <select class="form-select" >
-                      <option selected>-</option>
-                      <option value="1">Lulu</option>
-                      <option value="2">田晴瑄</option>
-                  </select>
-                  </div>
-                  </div>
-                  <button className="btn btn-originalgray" type="button">已付費</button>
-              </div>
-              <div className="form-group4"> 
-                <div className="form-group4-1">
-                    <label for="exampleInputEmail1">學員2:</label>
-                    <div className="select">
-                      <select class="form-select " >
-                          <option selected>-</option>
-                          <option value="1">Lulu</option>
-                          <option value="2">田晴瑄</option>
-                      </select>
-                    </div>
-                </div>
-                <button className="btn btn-originalgray" type="button">未付費</button>
-              </div>
-              <div className="form-group">
-                  <label for="exampleInputEmail1">堂數:</label>
-                  <div className="select">
-                    <select class="form-select" >
-                        <option selected>-</option>
-                        <option value="1">1</option>
-                        <option value="10">10</option>
-                        <option value="20">20</option>
-                    </select>
-                  </div>
-              </div>
-              <div class="form-group">
-                  <label for="exampleInputEmail1">堂薪:</label>
-                  <div className="select">
-                    <input type="text" class="form-select"></input>
-                  </div>
-              </div>
               
-              <div class="form-group">
-                  <label  className="" for="exampleInputEmail1">體驗課:</label>
-                  <div className=" check">
-                    <div class="form-check">
-                      <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" value='true' ></input>
-                      <label class="form-check-label" for="flexRadioDefault1">
-                        是
-                      </label>
+               {/* 場租 */}
+              <div className={toggle === 4 ? "show-content":"content"}>
+                <div className="class_category">
+                        <div className="form-group">
+                            <label  for="exampleInputEmail1">教練:</label>
+                            <div className="select">
+                              <select className="form-select " >
+                                  <option selected>-</option>
+                                  <option value="A">A</option>
+                                  <option value="B">B</option>
+                                  <option value="C">C</option>
+                              </select>
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <label  for="exampleInputEmail1">樓層:</label>
+                            <div className="select">
+                              <select className="form-select " >
+                                  <option selected>-</option>
+                                  <option value="1">1樓</option>
+                                  <option value="2">2樓</option>
+                                  <option value="3">3樓</option>
+                                  <option value="4">4樓</option>
+                              </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">日期:</label>
+                            <div className="select">
+                              <input type="text" class="form-select"></input>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">時間:</label>
+                            <div className="select">
+                              <input type="text" class="form-select"></input>
+                            </div>
+                        </div>
+                        <div class="form-group2">
+                            <label for="exampleInputPassword1">備註:</label>
+                            <div className="select">
+                              <textarea class="form-select" id="exampleFormControlTextarea1" rows="3" ></textarea>
+                            </div>  
+                        </div>
+                        <div class="form-group3">
+                          <button type="submit" class="btn btn-golden" >新增</button>
+                        </div>
                     </div>
-                    <div class="form-check">
-                      <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" value='false' ></input>
-                      <label class="form-check-label" for="flexRadioDefault2">
-                        否
-                      </label>
-                  </div>
               </div>
-              </div>
-              <div class="form-group2">
-                  <label for="exampleInputPassword1">備註:</label>
-                  <div className="select">
-                    <textarea class="form-select" id="exampleFormControlTextarea1" rows="3" ></textarea>
-                  </div>  
-              </div>
-              <div class="form-group3">
-                <button type="submit" class="btn btn-golden" >新增</button>
-              </div>
+             
+              
             </form>
             
          
