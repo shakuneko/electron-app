f5cded0f85d9eb586155270dd36e601b1fffa80a
Chun-Han-Lin <hanklin0208@gmail.com>
2023-09-26 13:02:23 +0800

    feat: btn place fix & yarn add luxon

--- src/renderer/src/components/StudentDetailCollapse.jsx
+++ src/renderer/src/components/StudentDetailCollapse.jsx
@@ -7,20 +7,31 @@ import Button from 'react-bootstrap/Button'
 import { useState } from 'react'
 function StudentDetailCollapse({ stuData }) {
   return (
-    <div style={
-        {
-        padding: "10px",
-        margin: "10px",
-       }
-    }>
-      <div>姓名：{stuData.stuName}</div>
-      <div>性別：{stuData.stuGender}</div>
-      <div>電話：{stuData.stuPhone}</div>
-      <div>Email：{stuData.stuEmail}</div>
-      <div>地址：{stuData.stuAddress}</div>
-      <div>緊急連絡人：{stuData.stuContact}</div>
-      <div>緊急連絡人電話：{stuData.stuContact_tel}</div>
-      <div>備註：{stuData.stuNote}</div>
+    <div
+      style={{
+        
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      <div style={{
+        marginBottom: '10px',
+        display: 'flex'
+      }}>
+        {/* <div className='classCoachBox-item'>姓名：{stuData.stuName}</div>
+      <div className='classCoachBox-item'>性別：{stuData.stuGender}</div> */}
+        <div className="classCoachBox-item">電話：{stuData.stuPhone}</div>
+        <div className="classCoachBox-item">Email：{stuData.stuEmail}</div>
+        <div className="classCoachBox-item">地址：{stuData.stuAddress}</div>
+      </div>
+      <div style={{
+        display: 'flex',
+      
+      }}>
+        <div className="classCoachBox-item">緊急連絡人：{stuData.stuContact}</div>
+        <div className="classCoachBox-item">緊急連絡人電話：{stuData.stuContact_tel}</div>
+        <div className="classCoachBox-item">備註：{stuData.stuNote}</div>
+      </div>
     </div>
   )
 }
