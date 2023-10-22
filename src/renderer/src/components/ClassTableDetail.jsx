265a178a559daec25652be9b10ad26c7483cdf33
Singularity-v <crystalwu.home@gmail.com>
2023-10-22 23:30:42 +0800

    feat-classTableDetail delete btn

--- src/renderer/src/components/ClassTableDetail.jsx
+++ src/renderer/src/components/ClassTableDetail.jsx
@@ -1,9 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { MaterialReactTable } from 'material-react-table';
 import { Link ,useParams} from 'react-router-dom';
 import { getStatusText } from './TableSelectOptions'
 import { useDispatch, useSelector } from 'react-redux';
 import { selectFileName,updateClassStatus } from '../redux/reducers/saveSlice'
+
+//confirm
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 function ClassTableDetail({classes}) {
     const dispatch = useDispatch();
@@ -16,141 +23,203 @@ function ClassTableDetail({classes}) {
    // 判断是否两个类别都为空
    const bothCategoriesEmpty = !hasCoachData && !hasStudentData;
 
-    useEffect(() => {
-      const updatedClassStatus = classes.map((item, index) => {
-        const nowStatus = getStatusText(item.buyDate);
-        if (item.status !== nowStatus && item.courseType !="場地租借") {
+  useEffect(() => {
+    const updatedClassStatus = classes.map((item, index) => {
+      const nowStatus = getStatusText(item.buyDate);
+      if (item.status !== nowStatus && item.courseType !="場地租借") {
+        return {
+          ...item,
+          status: nowStatus,
+        };
+      }
+      else if (item.courseType =="場地租借"){
           return {
             ...item,
-            status: nowStatus,
+            status: "無期限",
           };
         }
-        else if (item.courseType =="場地租借"){
-            return {
-              ...item,
-              status: "無期限",
-            };
-          }
-        return item;
-      });
-      console.log("newClassStatus", updatedClassStatus);
-      dispatch(updateClassStatus(updatedClassStatus));
-      setNewClassStatus(updatedClassStatus);
-    }, []);
-    console.log("newClassStatus", newClassStatus);
+      return item;
+    });
+    console.log("newClassStatus", updatedClassStatus);
+    dispatch(updateClassStatus(updatedClassStatus));
+    setNewClassStatus(updatedClassStatus);
+  }, []);
 
-    const AddBGC = ({cell}) => { // 設定邊框
-        let e;
-    
-        if (cell.getValue()  == '快要截止') e = <span style={{backgroundColor:"#dee2e6", padding:"10px", borderRadius:'5px'}}>{cell.getValue()}</span>
-        else if (cell.getValue()  == '已截止') e = <span style={{backgroundColor:"#F16D6D", padding:"10px", borderRadius:'5px', color:"white"}}>{cell.getValue()}</span>
-        else if (cell.row.original.courseType == '場地租借') e = <span>無期限</span>
-        else e = <div>-</div>
-        return e
+  console.log("newClassStatus", newClassStatus);
+  const [open, setOpen] = useState(false);
+  const [rowToDelete, setRowToDelete] = useState(null);
+
+  const handleClickOpen = (row) => {
+    setRowToDelete(row); // 保存要删除的行数据
+    setOpen(true);
+  };
+  
+  const handleClose = () => {
+    setRowToDelete(null); // 关闭对话框时重置要删除的行数据
+    setOpen(false);
+  };
+
+  const handleDeleteConfirmed = () => {
+    if (rowToDelete) {
+      // 执行删除操作
+      handleDeleteRow(rowToDelete);
+
+      // 清除要删除的行数据和关闭对话框
+      setRowToDelete(null);
+      setOpen(false);
     }
-    
-    const CheckOut = ({renderedCellValue}) => { //設定查看按鈕要進入的頁面
-        return<>
-           <Link to={`/classes/id/${renderedCellValue}`} className='table-link-underline-none'>
-                <button type="button" className="btn btn-golden">查看</button> 
-            </Link>
-        </>
-    };
-    
-    const AddAlertMode = ({renderedCellValue}) => {
-        if (renderedCellValue === '1') return <span className="alert-mode">{renderedCellValue}</span>;
-        else return<>{renderedCellValue}</>
+  };
+  const handleDeleteRow = useCallback( //  儲存刪除
+    (row) => {
+      // setTableData([...classes]);
+      const newTableData = classes.filter((item, index) => index !== row.index);
+      console.log("newTableData data", newTableData);
+      dispatch(updateClassStatus(newTableData));
+      setNewClassStatus(newTableData);
+    },
+    [classes],
+  );
+
+  const AddBGC = ({cell}) => { // 設定邊框
+    let e;
+
+    if (cell.getValue()  == '快要截止') e = <span style={{backgroundColor:"#dee2e6", padding:"10px", borderRadius:'5px'}}>{cell.getValue()}</span>
+    else if (cell.getValue()  == '已截止') e = <span style={{backgroundColor:"#F16D6D", padding:"10px", borderRadius:'5px', color:"white"}}>{cell.getValue()}</span>
+    else if (cell.row.original.courseType == '場地租借') e = <span>無期限</span>
+    else e = <div>-</div>
+    return e
+  }
+  
+  const CheckOut = ({renderedCellValue, row}) => { //設定查看按鈕要進入的頁面
+    return<>
+      <div style={{display:"flex", flexDirection:"row"}}>
+        <Link to={`/classes/id/${renderedCellValue}`} className='table-link-underline-none'>
+            <button type="button" className="btn btn-golden">查看</button> 
+        </Link>
+        <button type="button" className="btn btn-danger" onClick={() => handleClickOpen(row)}>              
+          刪除
+        </button> 
+
+      </div>
+    </>
+  };
+  
+  const AddAlertMode = ({renderedCellValue}) => {
+    if (renderedCellValue === '1') return <span className="alert-mode">{renderedCellValue}</span>;
+    else return<>{renderedCellValue}</>
+  }
+
+  
+  // const inputDateString = "2023-8-30"; // 你的输入日期
+  // const result = getStatusText(inputDateString);
+  // console.log(result);
+
+  const columns = [ //表格有的資料
+    {
+        accessorKey:"classID",
+        header:"#",
+        size:50,
+    },
+    {
+        accessorFn: (row) => {
+            // !!row.coach && 
+            const newData = row.coach.map((item) => {
+                const coachs = []
+                coachs.push(item.coachName)
+                return coachs
+            })
+            return `${newData.join("、")} `
+          },
+        header:"教練",
+        filterVariant: 'select',
+        size:100,
+    },
+    {
+        accessorKey:"courseType",
+        header:"課程種類",
+        size:100,
+        filterVariant: 'select',
+        enableSorting: false
+    },
+    {
+        accessorFn: (row) => {
+            const newData = row.student.map((item) => {
+                const students = []
+                students.push(item.stuName)
+                return students
+            })
+            return `${newData.join("、")} `
+          },
+        header:"學員",
+        size:100,
+        enableSorting: false,
+        filterVariant: 'select',
+    },
+    {
+        accessorKey:"courseLeft",
+        header:"剩餘堂數",
+        size:100,
+        Cell: AddAlertMode
+    },
+    {
+        accessorKey:"coursesAll",
+        header:"總堂數",
+        size:100,
+        Cell: AddAlertMode
+    },
+    {
+        accessorKey:"exCourse",
+        header:"體驗課",
+        size:50,
+        Cell: ({ renderedCellValue }) => {
+            if (renderedCellValue === '是') return <span className="alert-mode">{renderedCellValue}</span>;
+            else return<>{renderedCellValue}</>
+            
+        },
+      
+    },
+    {
+        accessorKey:"status",
+        header:"狀態",
+        size:100,
+        Cell: AddBGC,
+        filterVariant: 'select',
+    },
+    {
+        accessorKey:"classID",
+        id:"checkout_id",
+        header:"操作",
+        size:100,
+        Cell: CheckOut,
+        enableSorting: false
     }
-
-    
-    // const inputDateString = "2023-8-30"; // 你的输入日期
-    // const result = getStatusText(inputDateString);
-    // console.log(result);
-
-    const columns = [ //表格有的資料
-          {
-            accessorKey:"classID",
-            header:"#",
-            size:50,
-        },
-        {
-            accessorFn: (row) => {
-                // !!row.coach && 
-                const newData = row.coach.map((item) => {
-                    const coachs = []
-                    coachs.push(item.coachName)
-                    return coachs
-                })
-                return `${newData.join("、")} `
-              },
-            header:"教練",
-            filterVariant: 'select',
-            size:100,
-        },
-        {
-            accessorKey:"courseType",
-            header:"課程種類",
-            size:100,
-            filterVariant: 'select',
-            enableSorting: false
-        },
-        {
-            accessorFn: (row) => {
-                const newData = row.student.map((item) => {
-                    const students = []
-                    students.push(item.stuName)
-                    return students
-                })
-                return `${newData.join("、")} `
-              },
-            header:"學員",
-            size:100,
-            enableSorting: false,
-            filterVariant: 'select',
-        },
-        {
-            accessorKey:"courseLeft",
-            header:"剩餘堂數",
-            size:100,
-            Cell: AddAlertMode
-        },
-        {
-            accessorKey:"coursesAll",
-            header:"總堂數",
-            size:100,
-            Cell: AddAlertMode
-        },
-        {
-            accessorKey:"exCourse",
-            header:"體驗課",
-            size:100,
-            Cell: ({ renderedCellValue }) => {
-                if (renderedCellValue === '是') return <span className="alert-mode">{renderedCellValue}</span>;
-                else return<>{renderedCellValue}</>
-                
-            },
-          
-        },
-        {
-            accessorKey:"status",
-            header:"狀態",
-            size:100,
-            Cell: AddBGC,
-            filterVariant: 'select',
-        },
-        {
-            accessorKey:"classID",
-            id:"checkout_id",
-            header:"操作",
-            size:50,
-            Cell: CheckOut,
-            enableSorting: false
-        }
-    ];
+  ];
     
   return (
-    
-    <MaterialReactTable 
+    <div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title" style={{ fontWeight:900}}>確認刪除？</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            確定要刪除這項資料嗎？
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions style={{ marginBottom:"8px" }}>
+          <button onClick={handleClose} className='btn btn-outline-dbrown'>
+            取消
+          </button>
+          <button onClick={handleDeleteConfirmed} className='btn btn-golden'>
+            確認
+          </button>
+        </DialogActions>
+      </Dialog>
+
+      <MaterialReactTable 
         columns={columns}
         data={newClassStatus} 
         initialState={{ 
@@ -188,7 +257,10 @@ function ClassTableDetail({classes}) {
 
         )}
                                 
-    />
+      />
+    </div>
+    
+   
                 
   )
 }
--- src/renderer/src/components/CoachTableDetail.jsx
+++ src/renderer/src/components/CoachTableDetail.jsx
@@ -1,21 +1,67 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { MaterialReactTable } from 'material-react-table';
 import { Link } from 'react-router-dom';
-import { selectOptions, CheckOut } from './TableSelectOptions'
+import { useDispatch } from 'react-redux';
+import { updateClassStatus } from '../redux/reducers/saveSlice'
+
+//confirm
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 
 function CoachTableDetail({classes}) {
+    const dispatch = useDispatch();
+    const [open, setOpen] = useState(false);
+    const [rowToDelete, setRowToDelete] = useState(null);
 
-    
-    const CheckOut = ({renderedCellValue}) => { //設定查看按鈕要進入的頁面
-        
+    const handleClickOpen = (row) => {
+        setRowToDelete(row); // 保存要删除的行数据
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setRowToDelete(null); // 关闭对话框时重置要删除的行数据
+        setOpen(false);
+    };
+
+    const handleDeleteConfirmed = () => {
+        if (rowToDelete) {
+        // 执行删除操作
+        handleDeleteRow(rowToDelete);
+
+        // 清除要删除的行数据和关闭对话框
+        setRowToDelete(null);
+        setOpen(false);
+        }
+    };
+    const handleDeleteRow = useCallback( //  儲存刪除
+        (row) => {
+        // setTableData([...classes]);
+        const newTableData = classes.filter((item, index) => index !== row.index);
+        console.log("newTableData data", newTableData);
+        dispatch(updateClassStatus(newTableData));
+        setNewClassStatus(newTableData);
+        },
+        [classes],
+    );
+
+    //操作BTN
+    const CheckOut = ({renderedCellValue, row}) => { //設定查看按鈕要進入的頁面
         return<>
-           <Link to={`/coach/name/${renderedCellValue}`} className='table-link-underline-none'>
+            <div style={{display:"flex", flexDirection:"row"}}>
+            <Link to={`/coach/name/${renderedCellValue}`} className='table-link-underline-none'>  
                 <button type="button" className="btn btn-golden">查看</button> 
             </Link>
+            <button type="button" className="btn btn-danger" onClick={() => handleClickOpen(row)}>              
+                刪除
+            </button> 
+            </div>
         </>
-    };
-    
+        };
+
     const columns = [ //表格有的資料
         {
             accessorKey:"coachID",
@@ -45,7 +91,7 @@ function CoachTableDetail({classes}) {
             accessorFn: (row) => `${row.major.join("、")} `,
             id:"major",
             header:"能帶課程",
-            size:150,
+            size:200,
             Cell: ({ renderedCellValue }) => (<span>{renderedCellValue}</span>)
             
         },
@@ -66,41 +112,60 @@ function CoachTableDetail({classes}) {
 
         }
     ];
-    
-    
-
 
   return (
-    
-    <MaterialReactTable 
-        columns={columns}
-        data={classes} 
-        initialState={{ 
-            showGlobalFilter: true,
-            sorting: [
-                { id: 'coachID', desc: true }, //sort by classID in descending order by default
-            ],
-         }} //show filters by default
-        enableColumnActions={false} //no need for column actions if none of them are enabled
-        enableColumnFilters={false} //filtering does not work with memoized table body
-        
-        enableDensityToggle={false} //density does not work with memoized table body
-        enableFullScreenToggle={false}
-        enableHiding={false} //column hiding does not work with memoized table body
-        // enableSorting={false} //sorting does not work with memoized table body
-        enableStickyHeader
-        renderTopToolbarCustomActions={() => (
-            <Link to="/coach/form" className='table-link-underline-none'>
-                <button type="button" className="btn btn-golden">新增教練</button> 
-            </Link>
-        )}
-        muiSearchTextFieldProps={{
-            placeholder: "搜尋想查找的名稱、堂數",
-            sx: { minWidth: '300px' },
-          }}
+    <div>
+        <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+            <DialogTitle id="alert-dialog-title" style={{ fontWeight:900}}>確認刪除？</DialogTitle>
+            <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+                確定要刪除這項資料嗎？
+            </DialogContentText>
+            </DialogContent>
+            <DialogActions style={{ marginBottom:"8px" }}>
+            <button onClick={handleClose} className='btn btn-outline-dbrown'>
+                取消
+            </button>
+            <button onClick={handleDeleteConfirmed} className='btn btn-golden'>
+                確認
+            </button>
+            </DialogActions>
+        </Dialog>
+
+        <MaterialReactTable 
+            columns={columns}
+            data={classes} 
+            initialState={{ 
+                showGlobalFilter: true,
+                sorting: [
+                    { id: 'coachID', desc: true }, //sort by classID in descending order by default
+                ],
+            }} //show filters by default
+            enableColumnActions={false} //no need for column actions if none of them are enabled
+            enableColumnFilters={false} //filtering does not work with memoized table body
+            
+            enableDensityToggle={false} //density does not work with memoized table body
+            enableFullScreenToggle={false}
+            enableHiding={false} //column hiding does not work with memoized table body
+            // enableSorting={false} //sorting does not work with memoized table body
+            enableStickyHeader
+            renderTopToolbarCustomActions={() => (
+                <Link to="/coach/form" className='table-link-underline-none'>
+                    <button type="button" className="btn btn-golden">新增教練</button> 
+                </Link>
+            )}
+            muiSearchTextFieldProps={{
+                placeholder: "搜尋想查找的名稱、堂數",
+                sx: { minWidth: '300px' },
+            }}
                                 
-    />
-                
+        />
+    </div>
   )
 }
 
